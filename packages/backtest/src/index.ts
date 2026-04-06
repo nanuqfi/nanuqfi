9d7b81b11157d702bb2b62f7e0d7c3f99579e12a
RECTOR <rector@rectorspace.com>
2026-04-06 13:08:26 +0700

    chore(backtest): scaffold package with types

---
+++ packages/backtest/src/index.ts
@@ -0,0 +1,8 @@
+export type {
+  BacktestConfig,
+  BacktestResult,
+  BacktestDataPoint,
+  HistoricalDataPoint,
+  ProtocolMetrics,
+} from './types'
+export { DEFAULT_BACKTEST_CONFIG } from './types'
